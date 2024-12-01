67f5197ddc99df58624f26616502156cbb8213c1
thunyalukBBTS <thunyaluk.sa@kkumail.com>
2024-12-01 15:49:03 +0700

    add security func

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "admin", password: "1212312121", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
