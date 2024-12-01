0f5c8ae72ba9cdc2cbf045a2d15bce9333007860
thunyalukBBTS <thunyaluk.sa@kkumail.com>
2024-12-01 13:29:36 +0700

    add resource path articles, use link-to

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,11 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root "articles#index"
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show" 
+  
+  
+  # use this instead.
+  resources :articles
 end
