c4c390659b3c5f29a3bb64925f605be04174597c
thunyalukBBTS <thunyaluk.sa@kkumail.com>
2025-02-08 11:49:00 +0700

    delete github workflow

---
+++ db/migrate/20241219100237_add_author_to_artitles.rb
@@ -0,0 +1,5 @@
+class AddAuthorToArtitles < ActiveRecord::Migration[8.0]
+  def change
+    add_column :articles, :author, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_01_082640) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_19_100237) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+    t.string "author"
   end
 
   create_table "comments", force: :cascade do |t|
